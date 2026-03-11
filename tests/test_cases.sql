54ede8c3ec1b804eb13be07b587aaeff218e2414
hernandeztrejoirvinociel1-cyber <hernandez.trejo.irvin.ociel.1@gmail.com>
2026-03-10 22:29:34 -0600

    Update test_cases.sql

--- tests/test_cases.sql
+++ tests/test_cases.sql
@@ -1,1 +1,10 @@
+INSERT INTO developers (name)
+VALUES ('Square Enix'), ('Nintendo');
+
+INSERT INTO genres (name)
+VALUES ('RPG'), ('Adventure');
+
+INSERT INTO games (title, developer_id, genre_id)
+VALUES ('Final Fantasy', 1, 1),
+('Zelda', 2, 2);
 
